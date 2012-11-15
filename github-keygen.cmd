5fbd61d52cbee1036c2c3a0322bbe4fb5dc0ad34
Olivier Mengué <dolmen@cpan.org>
2012-11-15 01:36:06 +0100

    'master' is now deprecated. Switch to the 'release' branch!

--- github-keygen.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-:: Win32 wrapper using tools from the msysgit install
-setlocal
-:: Add %GIT_HOME% to %PATH%: this should provide perl.exe and ssh-keygen.exe
-:: Unfortunately msysgit only bundles perl 5.8.8 and no Pod::Usage
-:: See https://github.com/msysgit/msysgit/issues/61
-for %%f in (git.cmd) do for /D %%i in ("%%~dp$PATH:f..\bin") do path %PATH%;%%~fi
-::echo %PATH%
-perl %~dpn0 %*
